71e79eb61242e2b3ead22481907a3e525e56221f
HuyTapCode <148745344+CodeForFun2004@users.noreply.github.com>
2026-03-19 22:23:22 +0700

    Done fix bug view-user for commit alert templates before applying API

--- src/features/users/views/users-view.tsx
+++ src/features/users/views/users-view.tsx
@@ -1,33 +1,6 @@
-// import { SearchParams } from 'nuqs';
-// import UserListingPage from '../components/user-listing';
-// import { searchParamsCache } from '@/lib/searchparams';
-// import { Suspense } from 'react';
-// import PageContainer from '@/components/layout/page-container';
-// import { DataTableSkeleton } from '@/components/ui/table/data-table-skeleton';
+// src/features/users/views/UsersView.tsx
+import UserListingPage from '../components/user-listing';
 
-// export const metadata = {
-//   title: 'Admin: User Management'
-// };
-
-// export default async function UsersView(props: PageProps) {
-//   const searchParams = await props.searchParams;
-
-//   await searchParamsCache.parse(searchParams);
-
-//   return (
-//     <PageContainer
-//       scrollable={false}
-//       pageTitle='Stations'
-//       pageDescription='Manage stations (Server side table functionalities.)'
-//       // infoContent={stationInfoContent}
-//     >
-//       <Suspense
-//         fallback={
-//           <DataTableSkeleton columnCount={7} rowCount={8} filterCount={2} />
-//         }
-//       >
-//         <UserListingPage />
-//       </Suspense>
-//     </PageContainer>
-//   );
-// }
+export default async function UsersView() {
+  return <UserListingPage />;
+}
